85332155c26d4787b4023c396507cbc60faed273
Susan Lamberton <kylelamberton1@gmail.com>
2022-01-23 21:37:50 +0000

    add test

--- test.sh
+++ test.sh
@@ -10,7 +10,7 @@ done
 
 for num1 in ${num1s[@]}; do
   for num2 in ${num2s[@]}; do
-    if [ $(./calc.sh '+' $num1 $num2) -eq $(($num1 + $num2)) ] && [ $(./calc.sh '-' $num1 $num2) -eq $(($num1 - $num2)) ] && [ $(./calc.sh '*' $num1 $num2) -eq $(($num1 * $num2)) ] && [ $(./calc.sh '/' $num1 $num2) -eq $(($num1 / $num2)) ]; then
+    if [ $(./demo.sh '+' $num1 $num2) -eq $(($num1 + $num2)) ] && [ $(./demo.sh '-' $num1 $num2) -eq $(($num1 - $num2)) ] && [ $(./demo.sh '*' $num1 $num2) -eq $(($num1 * $num2)) ] && [ $(./calc.sh '/' $num1 $num2) -eq $(($num1 / $num2)) ]; then
       echo "$num1, $num2: test passed"
     else
       echo "$num1, $num2: test failed"
